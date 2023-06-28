be2781224bc487286c03344122056b8828921d17
CavaliereDavid <54360776+CavaliereDavid@users.noreply.github.com>
2023-06-29 00:04:14 +0200

    function slot machine

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let coins = 10;
-let score = 0;
+let coins = document.location.href.split('=')[1];
+
 // this vector represents the current situation
 let items = [
   [1, 2, 3],
@@ -12,7 +12,7 @@ let lookupTableSymbol = [
   '&heartsuit;',
   '&diamondsuit;',
   '&spadesuit;',
-  '&clubsuit',
+  '&clubsuit;',
   '7',
   '&dollar;',
   '&star;',
@@ -45,6 +45,7 @@ function drawSlotMachine() {
       // We need to select the correct block from the DOM
       let itemQuery = document.getElementById('item-' + i + j);
       itemQuery.innerHTML = lookupTableSymbol[items[i][j]];
+      itemQuery.style.color = lookupTableColor[items[i][j]];
     }
   }
 }
@@ -59,8 +60,47 @@ function spin() {
   for (i = 0; i < 3; i++) {
     for (j = 0; j < 3; j++) {
       // retrieves a random number between 0 and 8
-      items[i][j] = Math.random() * 9;
+      items[i][j] = Math.floor(Math.random() * 9);
     }
   }
   drawSlotMachine();
+  award();
+  let remainingCoins = document.getElementById('remaining-coins');
+  remainingCoins.innerHTML = 'gettoni rimasti: ' + coins;
 }
+
+function award() {
+  let message = document.getElementById('message');
+  if (
+    (items[0][0] == items[0][1] && items[0][1] == items[0][2]) ||
+    (items[1][0] == items[1][1] && items[1][1] == items[1][2]) ||
+    (items[2][0] == items[2][1] && items[2][1] == items[2][2]) ||
+    (items[0][0] == items[1][0] && items[1][0] == items[2][0]) ||
+    (items[1][0] == items[1][1] && items[1][1] == items[1][2]) ||
+    (items[2][0] == items[2][1] && items[2][1] == items[2][2]) ||
+    (items[0][0] == items[1][1] && items[1][1] == items[2][2]) ||
+    (items[0][2] == items[1][1] && items[1][1] == items[0][2])
+  ) {
+    coins = coins * 2;
+    message.innerHTML = 'Complimenti, hai vinto!';
+  } else {
+    coins = coins - 1;
+    if (coins > 0) {
+      message.innerHTML = 'Riprova';
+    } else {
+      message.innerHTML =
+        'Gettoni esauriti. Torna alla <a href="index.html">pagina principale</a>';
+      document.querySelector('#spin').disabled = true;
+    }
+  }
+}
+
+/**
+ * @todo aggiornare i punteggi e i gettoni rimasti
+ * per html usare flex o grid
+ * array di oggetti proprietà symbol e color
+ * ogni colonna deve essere un array
+ * realizzare un array di 3 elementi che man mano toglie il primo e ne aggiunge uno random in fondo
+ * usare setInterval e la funzione clearInterval per interrompere il movimento
+ * poi realizzare la funzione per la verifica della vincità
+ */
